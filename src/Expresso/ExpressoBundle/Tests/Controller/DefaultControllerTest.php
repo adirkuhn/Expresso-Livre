ffa5f8ee586aa735a75c49ae003da8bf3e95cc17
Adir Kuhn <adirkuhn@gmail.com>
2012-07-12 10:59:48 -0300

    Teste para build com o travis-ci

--- src/Expresso/ExpressoBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Expresso/ExpressoBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testConfigExpresso()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/configExpresso.php');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Configurador do Expresso")')->count() > 0);
     }
 }
